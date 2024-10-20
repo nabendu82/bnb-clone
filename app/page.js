5cc412a479bf2f9b4f975aee02c9cbc899876971
nabendu82 <nabendu.biswas@gmail.com>
2024-10-20 16:46:17 +0530

    Appwrite integration done

--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
-export default function Home() {
+import Heading from '@/components/Heading';
+import RoomCard from '@/components/RoomCard';
+import getAllRooms from './actions/getAllRooms';
+
+export default async function Home() {
+  const rooms = await getAllRooms();
+
   return (
     <>
-      <h1>BnB App</h1>
+      <Heading title='Available Rooms' />
+      {rooms.length > 0 ? (
+        rooms.map(room => <RoomCard room={room} key={room.$id} />)
+      ) : (
+        <p>No rooms available at the moment</p>
+      )}
     </>
   );
 }
